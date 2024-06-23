ba252e202cf677a360e7c620ec947e3b60ed89dc
Chase Counsell <cjcounsell@gmail.com>
2024-06-23 10:38:52 -0400

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install software-properties-common git ansible curl zip unzip
+sudo apt install software-properties-common git ansible curl zip unzip fd-find ripgrep
 ansible-playbook ./ubuntu.yml -JK
